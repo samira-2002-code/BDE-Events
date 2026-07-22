4bd68b78b9366c6853de6b179655538167e80585
samira-2002-code <samiraassemlali2@gmail.com>
2026-07-22 16:48:44 +0100

    worked on model event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'date',
+        'time',
+        'location',
+        'price',
+        'capacity',
+        'created_by',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class,'created_by');
+    }
+    public function reservations(){
+        return $this->hasMany(Reservation::class);
+    }
 }
+
+
+
+

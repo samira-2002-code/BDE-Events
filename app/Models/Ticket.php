6328633e5c15ee7eb949fac3110ffe137b6935b0
samira-2002-code <samiraassemlali2@gmail.com>
2026-07-22 20:22:37 +0100

    update ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'reservation_id',
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }

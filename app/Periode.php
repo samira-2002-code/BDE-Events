6383394a791470f39e64193e65313650bba184bd
samira-2002-code <samiraassemlali2@gmail.com>
2026-08-03 14:56:34 +0100

    creat class periode

---
+++ app/Periode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Container\Attributes\DB;
+
+
+
+class Periode
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+       
+    }
+    public function PeriodReservation(){
+         $periode = new Periode;
+         
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\TicketController;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', [EventController::class, 'index'])->name('home');
-
-Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+Route::get('/dashboard', [AdminController::class, 'dashboard'])
+    ->name('dashboard');
 
 Route::resource('events', EventController::class)->except(['show']);
 
@@ -19,18 +19,18 @@ Route::post('/reservations', [ReservationController::class, 'store'])
 Route::get('/reservations', [ReservationController::class, 'index'])
     ->name('reservations.index');
 
-    Route::get('/tickets', [TicketController::class, 'index'])
+Route::get('/tickets', [TicketController::class, 'index'])
     ->name('tickets.index');
 
 Route::get('/tickets/{ticket}', [TicketController::class, 'show'])
     ->name('tickets.show');
 
 
-Route::get("/login",function(){
+Route::get("/login", function () {
     return view("/auth/login");
 })->name("login");
-Route::post("/login",[AuthController::class,"login"])->name("login");
-Route::get("/register",function(){
+Route::post("/login", [AuthController::class, "login"])->name("login");
+Route::get("/register", function () {
     return view("/auth/register");
 })->name("register");
-Route::post("/register",[AuthController::class,"register"])->name("register");
+Route::post("/register", [AuthController::class, "register"])->name("register");
